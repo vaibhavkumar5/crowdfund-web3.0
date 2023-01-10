a630b8764ae3bd1784ef9de0ff23538889fbecef
vaibhav kumar <77304656+vaibhavkumar5@users.noreply.github.com>
2023-01-10 18:39:55 +0530

    first commit

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <ThirdwebProvider desiredChainId={ChainId.Goerli}>
+
+    </ThirdwebProvider>
+)
