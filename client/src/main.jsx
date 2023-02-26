f1eee3a9da9c338e753abd5265680c2c2e107282
vaibhav kumar <77304656+vaibhavkumar5@users.noreply.github.com>
2023-02-27 02:27:51 +0530

    sidebar

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 
+import App from './App';
+import './index.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <ThirdwebProvider desiredChainId={ChainId.Goerli}>
-
+        <Router>
+            <App />
+        </Router>
     </ThirdwebProvider>
 )
